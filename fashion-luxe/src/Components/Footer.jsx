fda4147fe2961ca8e0518dbbe9b156b2f993e1b2
Itsmearun1 <arundevvb1@gmail.com>
2023-01-18 23:28:09 +0530

    day2-progress

---
+++ fashion-luxe/src/Components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import "./Footer.css"
+export const Footer = () => {
+  return (
+    <div className='footer-wrapper'>Footer</div>
+  )
+}
---
+++ fashion-luxe/src/Components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import "./Navbar.css"
+export const Navbar = () => {
+  return (
+    <div className='navbar-wrapper'>Navbar</div>
+  )
+}
